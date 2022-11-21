0e5494141e73003fe3b4b1bc914564787ea1fe86
souchan410 <113465510+souchan410@users.noreply.github.com>
2022-11-21 15:08:39 +0400

    added dev branch

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=pink> Ayyy Memes";
+echo "Made by <font color=pink> Ayy Memes";
 
 ?>
 
